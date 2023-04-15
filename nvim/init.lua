cde34e3c77f146b2e1fb5ec35423a0cf98801ec2
amsesk <amsesk@umich.edu>
2023-04-14 22:27:19 -0400

    update config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,41 +1,75 @@
--- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- Functional wrapper for mapping custom keybindings
--- (from https://blog.devgenius.io/create-custom-keymaps-in-neovim-with-lua-d1167de0f2c2)
-function map(mode, lhs, rhs, opts)
-    local options = { noremap = true }
-    if opts then
-        options = vim.tbl_extend("force", options, opts)
-    end
-    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+nullmodule = {}
+function nullmodule:setup(...)
+    return {}
 end
 
--- map("n", ",<Space>", ":nohlsearch<CR>", { silent = true })
+function try_require(module_name) 
+    local ok, module = pcall(require, module_name);
+    if ok then
+        return module
+    else    
+        return nullmodule 
+    end
+end
+-- Packer plugins
+require('plugins').install()
 
--- Set line numbers
+try_require'lualine'.setup {
+    options = { theme = try_require'lualine.themes.horizon' },
+}
+try_require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all" (the four listed parsers should always be installed)
+  ensure_installed = { "bash", "c", "lua", "vim", "help", "python", "r", "rust" },
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = true,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = false,
+
+  -- List of parsers to ignore installing (for "all")
+  ignore_install = { },
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    disable = { },
+    -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+    -- disable = function(lang, buf)
+    --    local max_filesize = 100 * 1024 -- 100 KB
+    --    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+    --    if ok and stats and stats.size > max_filesize then
+    --        return true
+    --    end
+    --end,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+}
+
+-- Nvim settings
 vim.o.number = true
-
--- make tabs shorter and use spaces
 vim.o.softtabstop = 4
 vim.o.expandtab = true
 vim.o.shiftwidth = 4
+vim.o.smartindent = true
 
--- set termguicolors to enable highlight groups
-vim.o.termguicolors = true
+-- Try to change the colorscheme, but do nothing if it fails
+local ok, _ = pcall(vim.cmd.colorscheme, 'zephyr')
 
-require('plugins')
+-- Nvim-R settings
+vim.api.nvim_set_var('R_assign', 0)
+vim.api.nvim_set_var('maplocalleader', "'")
 
--- empty setup using defaults
-require("nvim-tree").setup {
-	open_on_setup_file = true,
-}
-
-require('nvim-web-devicons').setup()
-
-require('lualine').setup {
-	options = {
-		theme = 'codedark',
-	}
-}	
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,17 +1,12 @@
-vim.cmd [[packadd packer.nvim]]
-
+--vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use {
-		'nvim-tree/nvim-tree.lua',
-		requires = {
-			'nvim-tree/nvim-web-devicons',
-		},
-		tag = 'nightly',
-	}
-	use 'jalvesaq/Nvim-R'
-	use {
-  		'nvim-lualine/lualine.nvim',
-  		requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-	}
+    use 'wbthomason/packer.nvim' 
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+    use 'nvim-treesitter/nvim-treesitter'
+    use 'glepnir/zephyr-nvim'
+    use 'jalvesaq/Nvim-R'
+    use 'gaalcaras/ncm-R'
 end)
