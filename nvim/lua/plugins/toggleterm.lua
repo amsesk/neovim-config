70e6aed7a935776bad62f9bd0558bbd6f613394b
AWS ParallelCluster user <ubuntu@ip-10-0-0-43.ec2.internal>
2024-01-31 00:05:49 +0000

    initial

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,14 @@
+K = {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = true,
+    opts = { 
+        size = 20,
+        open_mapping = "<leader>tt" ,
+        persist_mode = false,
+        direction = "horizontal",
+        start_in_insert = true,
+    }
+}
+
+return K
