0adef043a8f592bb2fec6dceb7218cbc68fccf6c
AWS ParallelCluster user <ubuntu@ip-10-0-0-43.ec2.internal>
2024-01-31 00:05:18 +0000

    update window keymaps to us wincmd

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -4,9 +4,8 @@ vim.keymap.set("n", "<leader>c", "\"_c")
 vim.keymap.set("n", "<leader>d", "\"_d")
 vim.keymap.set("v", "<leader>d", "\"_d")
 
-vim.keymap.set("t", "<leader>ww", "<C-\\><C-n><C-w>w")
-vim.keymap.set("n", "<leader>ww", "<C-W>w")
-vim.keymap.set("n", "<leader>wp", "<C-W>W")
+vim.keymap.set({"n","t"}, "<leader>ww", "<cmd>wincmd w<cr>")
+vim.keymap.set({"n","t"}, "<leader>wp", "<cmd>wincmd p<cr>")
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
