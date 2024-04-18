607dcb36771b7ae8769a9a18ef1e7b622d331290
amsesk <amsesk@oregonstate.edu>
2024-04-17 20:31:48 -0400

    add config.filetypes with hyprlang as first one

--- nvim/init.lua
+++ nvim/init.lua
@@ -4,6 +4,7 @@ vim.api.nvim_set_var('mapleader', " ")
 require("config.lazy")
 require("config.keymaps")
 require("config.settings")
+require("config.filetypes")
 require("snippets.python")
 
 local ok, _ = pcall(vim.cmd.colorscheme, 'rose-pine-moon')
