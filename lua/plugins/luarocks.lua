21c762248be29e4bfbb82a83cf8d0eaa0b203942
amsesk <amsesk@umich.edu>
2024-06-05 11:55:15 -0400

    rm magick rock

--- lua/plugins/luarocks.lua
+++ lua/plugins/luarocks.lua
@@ -2,7 +2,7 @@ K = {
   "vhyrro/luarocks.nvim",
   priority = 1001, -- Very high priority is required, luarocks.nvim should run as the first plugin in your config.
   opts = {
-      rocks = { "magick" },
+      -- rocks = { "magick" },
   },
 }
 return K
