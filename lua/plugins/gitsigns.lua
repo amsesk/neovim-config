804f1939a68bff52557ee57f4628e3d9fab456cf
amsesk <amsesk@upenn.edu>
2024-07-03 03:42:01 +0000

    add_keybinds

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,5 +1,11 @@
 K = {
     "lewis6991/gitsigns.nvim",
     config = true,
-}
+    lazy = false,
+    keys = {
+            { "<leader>hs", "<cmd>Gitsigns stage_hunk<cr>", desc = "Stage hunk", mode = {"n", "v"} },
+            { "<leader>hu", "<cmd>Gitsigns undo_stage_hunk<cr>", desc = "Undo stage hunk", mode = {"n", "v"} },
+            { "<leader>htl", "<cmd>Gitsigns toggle_linehl<cr>", desc = "Toggle line highlights.", mode = {"n", "v"} },
+        },
+    }
 return K
