a37be632d1fd3983cde47b2b7daef559d8639b0e
amsesk <amsesk@upenn.edu>
2025-06-12 14:25:16 +0000

    add_keybinds

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -3,6 +3,11 @@ K = {
     opts = {
         use_icons = false,
     },
+    keys = {
+        { "<leader>dvo", mode = { "n" }, "<cmd>DiffviewOpen<cr>", desc = "Open diffview" },
+        { "<leader>dvc", mode = { "n" }, "<cmd>DiffviewClose<cr>", desc = "Close diffview" },
+        { "<leader>dvh", mode = { "n" }, "<cmd>DiffviewFileHistory<cr>", desc = "Open Diffview file history" },
+    },
     config = true,
 }
 return K
