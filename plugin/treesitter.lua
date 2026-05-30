00e1e2d492938b3559e3874733e70b6fc2dbf175
amsesk <amsesk@upenn.edu>
2026-05-30 21:57:29 +0000

    add snakemake

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -2,7 +2,7 @@ vim.pack.add({
 	"https://www.github.com/nvim-treesitter/nvim-treesitter",
 })
 
-local langs = { "markdown", "markdown_inline", "r", "rnoweb", "yaml", "latex", "csv", "python", "lua" }
+local langs = { "markdown", "markdown_inline", "r", "rnoweb", "yaml", "latex", "csv", "python", "lua", "snakemake" }
 require("nvim-treesitter").install(langs)
 
 vim.api.nvim_create_autocmd("FileType", {
